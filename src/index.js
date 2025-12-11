453f520a8b3a794362db578c773dbf56a117c443
MarRam-1 <mariana.rmrz.96@gmail.com>
2025-12-10 18:06:04 -0600

    Added the AI PI integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "sdsdfdf",
+    strings: response.data.answer,
     autoStart: true,
     delay: 2,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#text-input");
+  let apiKey = "e439o7e5c26cc43ac0tef7fd04f093b8";
+  let prompt = `Generate a calaverita literaria about ${instructionsInput.value}`;
+  let context =
+    "You are an expert on calaveritas literarias, your mision is to generate a 8 line calaverita in basic HTML, without saying is html, and separate each line with <br /> and separate each paragraph with double <br /> , sign the calaverita with 'SheCodesAI' inside a <strong> element at the end of the calaverita.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+
+  let poemElement = document.querySelector("#poem");
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
